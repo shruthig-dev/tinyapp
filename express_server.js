be3bd895c10aa44745b61a26ce7fe9752c97ea60
vagrant <shruthigangadharblr@gmail.com>
2020-08-31 22:16:06 +0000

    Added urls_new file and Submit request url form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,6 +18,9 @@ app.get("/urls", (req, res) => {
   let templateVars = {urls:urlDatabase};
   res.render("urls_index",templateVars);
 });
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -24,7 +29,20 @@ app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
-
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+    var result = '';
+    const char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (var i = 6; i > 0; --i) 
+    {
+        result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+}

3fbea6a81dfb5d419b7c37c2ac03ecac4d0ca21b
vagrant <shruthigangadharblr@gmail.com>
2020-09-01 16:51:37 +0000

    Added URL Delete action
    Added URL validation method

--- express_server.js
+++ express_server.js
@@ -19,37 +19,89 @@ app.get("/urls", (req, res) => {
   res.render("urls_index",templateVars);
 });
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+
+  if (templateVars.longURL !== undefined || null) {
+    res.redirect(templateVars.longURL);
+  } else {
+    res.send("Not Found");
+  }
+
+  //res.render("urls_show", templateVars);
 });
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
-     const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL !== undefined || null) {
     res.redirect(longURL);
-  });
+  } else {
+    res.send("Not Found");
+  }
+    
+});
 app.post("/urls", (req, res) => {
-    console.log(req.body);  
-    //res.send("Ok");       
-    //let newUrlId = generateRandomString();
-    let newUrl = urlDatabase["b2xVn2"];
-    res.redirect(newUrl);
-  });
+  
+  let newUrlId = generateRandomString();
+  let formattedUrl = formatUrl(req.body.longURL);
+  
+  urlDatabase[newUrlId] = formattedUrl;
+  let newUrl = urlDatabase[newUrlId];
+
+  if (newUrl !== undefined || null) {
+    let templateVars = {urls:urlDatabase};
+    res.render("urls_index",templateVars);
+  } else {
+    res.send("Error : Not able to create new url");
+  }
+});
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const id = req.params.shortURL;
+  
+  if (urlDatabase.hasOwnProperty(id)) {
+    delete urlDatabase[id];
+    if (!urlDatabase.hasOwnProperty(id)) {
+     
+      res.redirect("/urls");
+    } else {
+      res.send("Delete operation failed.");
+    }
+  } else {
+    res.send("Not Found");
+  }
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
-    var result = '';
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    for (var i = 6; i > 0; --i) 
-    {
-        result += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return result;
-}
+let generateRandomString = () => {
+  let result = '';
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 6; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
+};
+let formatUrl = (url) => {
+  if (validURL(url)) {
+    return url;
+  } else {
+    url = 'https://' + url;
+  }
+  return url;
+};
+let validURL = (url) => {
+  let pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(url);
+};
